82d5955b9a848c0d20d005993dc5b106c42e122f
NendiSofiandy <nendisofiandy80@gmail.com>
2025-05-18 10:02:57 +0700

    update

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.post('/settings', async (req, res) => {
         await queue.add(async () => {
             const payload = req.body
             const data = await settingDevice(payload)
+            res.status(200).json(data)
         })
-        res.status(200).json(data)
     } catch (error) {
         res.status(400).send(error)
     }
